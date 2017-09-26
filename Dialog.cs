18465beb2d503cb75ce9bf105b61e23dc4e367db
Neta <admin@nsys.pw>
2017-09-26 13:51:40 +0200

    Initial commit

---
+++ Dialog.cs
@@ -0,0 +1,22 @@
+﻿using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Managers;
+
+
+namespace GameDialog
+{
+    public class Dialog : Script
+    {
+		[Command("opendialog")]
+		public void OpenDialog(Client sender, string title, string message, string btnLeft, bool blockControls = false, string btnRight = "")
+		{
+			API.triggerClientEvent(sender, "openDialog", btnLeft, btnRight, title, message, blockControls);
+		}
+
+		[Command("closedialog")]
+		public void CloseDialog(Client sender)
+		{
+			API.triggerClientEvent(sender, "closeDialog");
+		}
+	}
+}
